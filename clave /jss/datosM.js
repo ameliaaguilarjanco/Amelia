e0f09c3eea51323e9988b0129cfe6c53065316e8
ameliaaguilarjanco <aguilarjancoamelia@gmail.com>
2024-11-08 13:13:17 -0300

    Add files via upload
    mi trabajo de web

---
+++ clave /jss/datosM.js
@@ -0,0 +1,12 @@
+function saber(){
+	var tu=document.getElementById('m1').value;
+	var td=document.getElementById('m2').value;
+	if(tu=="amelia")
+	{if (td=="ame")
+		{alert("DATOS CORRECTOS");
+         window.location.href="inicio.html";
+	}}
+	else{
+		alert("DATOS INCORRECTOS");
+	}
+}
